68db740ec58ad6846e04aa9ada003ec654f199c1
zxwnstn@naver.com <zxwnstn@naver.com>
2018-12-05 01:04:17 +0900

    1_2강 정리

---
+++ 1_2ProcessExe_SPconcept_Bus/1_2_3Bus.cpp
@@ -0,0 +1,22 @@
+/*
+
+★I/O Bus Sytem의 간략한 구조
+
+1. Data Bus
+
+	데이터들을 주고받는다. (연산자, 피연산자등)
+
+2. Adress Bus
+
+	Main memory상의 Data 주소 정보를 주고받는다.
+
+3. Control Bus
+
+	작업의 처리 순서와 데이터를 줄지 받을지에 대한 신호를 주고받는다.
+
+*/
+
+int main()
+{
+	return 0;
+}
